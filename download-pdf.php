ed0c4e2ca1c90587a186f5ba3d713f2e492ab993
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-04-19 11:28:50 +0300

    more to previous

--- download-pdf.php
+++ download-pdf.php
@@ -7,7 +7,7 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 
 $options = new Options();
-$options->set('defaultFont', 'DejaVu Sans');
+// $options->set('defaultFont', 'DejaVu Sans');
 $options->set('isRemoteEnabled', true);
 
 $dompdf = new Dompdf($options);
