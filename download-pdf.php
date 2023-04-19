b560722f94e7011fe7cefc0e92a80999b9e4330c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-04-19 14:15:12 +0300

    keep western numbers as them 9323243  instead of ٩٣٢٣٢٤٣

--- download-pdf.php
+++ download-pdf.php
@@ -13,6 +13,8 @@ $options->set('isRemoteEnabled', true);
 $dompdf = new Dompdf($options);
 $arabic = new \ArPHP\I18N\Arabic();
 
+$absoluteUrl = url() . '/assets';
+
 $reportHtml = "
 <!DOCTYPE html>
 <html>
@@ -24,7 +26,7 @@ $reportHtml = "
     <style>
         @font-face {
             font-family: 'eazycare';
-            src: url(/Users/watheq/Sites/htdocs/ar-dom-pdf/assets/cocon-next-arabic.ttf);
+            src: url({$absoluteUrl}/cocon-next-arabic.ttf);
             font-weight: normal;
             font-style: normal;
             font-variant: normal;
@@ -96,14 +98,14 @@ $reportHtml = "
         }
     </style>
 </head>
-<body dir='rtl' style='background: url(/Users/watheq/Sites/htdocs/ar-dom-pdf/assets/background.jpg) ;background-size: 100% 100%;'>
+<body dir='rtl' style='background: url({$absoluteUrl}/background.jpg) ;background-size: 100% 100%;'>
 <div class='header'>
     <div class='invoice-name'>
         <p>الفاتورة </p>
         <span>رقم الطلب: 9323243</span>
     </div>
     <div class='logo'>
-        <img src='/Users/watheq/Sites/htdocs/ar-dom-pdf/assets/vertical@4x-8.png' width='140'>
+        <img src='{$absoluteUrl}/vertical@4x-8.png' width='140'>
     </div>
 </div>
 <div class='body'>
@@ -146,14 +148,20 @@ $maxLineChr = 80;
 $htmlOutput = substr($reportHtml, 0, $p[0]);
 
 for ($i = 0; $i < count($p); $i += 2) {
-    $utf8ar = $arabic->utf8Glyphs(substr($reportHtml, $p[$i], $p[$i+1] - $p[$i]), $maxLineChr);
-    $lastEn = isset($p[$i+2]) ? $p[$i+2] : strlen($reportHtml);
+    $utf8ar = $arabic->utf8Glyphs(substr($reportHtml, $p[$i], $p[$i + 1] - $p[$i]), $maxLineChr, hindo: false);
+    $lastEn = isset($p[$i + 2]) ? $p[$i + 2] : strlen($reportHtml);
 
     $htmlOutput .= str_replace("\n", "<br>\n", $utf8ar);
-    $htmlOutput .= substr($reportHtml, $p[$i+1], $lastEn - $p[$i+1]);
+    $htmlOutput .= substr($reportHtml, $p[$i + 1], $lastEn - $p[$i + 1]);
 }
 
 $dompdf->loadHtml($htmlOutput);
 $dompdf->setPaper('A4', 'landscape');
 $dompdf->render();
 $dompdf->stream('example', array('Attachment' => false));
+
+
+function url()
+{
+    return 'http://' . $_SERVER['HTTP_HOST'];
+}
